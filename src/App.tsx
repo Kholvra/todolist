f56cefc5a92c625d935e545f08b60ee36bce411a
kolvra <sabrisirajkholbiabby@gmail.com>
2025-08-27 11:13:23 +0700

    tittle & desc template

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,15 @@ import Task from "./Components/Task";
 
 function App() {
   return (
-    <>
-      <Task />
-    </>
+    <div className="h-screen flex justify-center items-center">
+      <div className="h-3/4 w-1/4 mx-auto border rounded-md shadow-md">
+        <div>
+          <h1>Todo List Title</h1>
+          <h2>Todo List Desc</h2>
+          <Task />
+        </div>
+      </div>
+    </div>
   );
 }
 
