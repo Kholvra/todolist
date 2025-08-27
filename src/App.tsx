228f6998319fa6c41a7b948ce80c98e8914efe87
kolvra <sabrisirajkholbiabby@gmail.com>
2025-08-27 16:25:11 +0700

    using props

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,33 @@
+import { useState, type MouseEvent } from "react";
 import Task from "./Components/Task";
 
 function App() {
+  const [taskItems, setTaskItems] = useState([
+    {
+      name: "abby",
+      status: false,
+      id: 1,
+    },
+    {
+      name: "Nadia",
+      status: true,
+      id: 2,
+    },
+  ]);
+
+  const toggleTaskStatus = (
+    event: MouseEvent<HTMLButtonElement>,
+    id: number
+  ) => {
+    console.log(event.currentTarget, id);
+    setTaskItems((prev) =>
+      prev.map((item) => {
+        if (item.id === id) return { ...item, status: !item.status };
+        return item;
+      })
+    );
+  };
+
   return (
     <div className="h-screen flex justify-center items-center">
       <div className="size-full lg:h-3/4 lg:w-1/4 mx-auto lg:border rounded-md shadow-md">
@@ -9,7 +36,7 @@ function App() {
             <h1>Todo List Title</h1>
             <h2>Todo List Desc</h2>
           </div>
-          <Task />
+          <Task tasks={taskItems} onToggle={toggleTaskStatus} />
         </div>
       </div>
     </div>
--- src/Components/Task.tsx
+++ src/Components/Task.tsx
@@ -1,42 +1,33 @@
-import { useState, type MouseEvent } from "react";
+  import type { MouseEvent } from "react";
+  
+  type taskType = {
+    id: number,
+    name: string,
+    status: boolean
+  }
 
-function Task() {
-  const [taskItems, setTaskItems] = useState([
-    {
-      name: "abby",
-      status: false,
-      id: 1,
-    },
-    {
-      name: "Nadia",
-      status: true,
-      id: 2,
-    },
-  ]);
+  type taskProps = {
+    tasks: taskType[],
+    onToggle: (e:MouseEvent<HTMLButtonElement>,id:number)=>void
+  }
+
+  function Task({tasks, onToggle}:taskProps) {
+
 
   const image = {
     checkmark: "../public/images/checkmark.svg",
     notdone: "../public/images/notdone.svg",
   };
 
-  function taskHandler(event: MouseEvent<HTMLButtonElement>, id: number) {
-    console.log(event.currentTarget, id);
-    setTaskItems((prev) =>
-      prev.map((item) => {
-        if (item.id === id) return { ...item, status: !item.status };
-        return item;
-      })
-    );
-  }
 
-  const itemList = taskItems.map((item) => {
+  const itemList = tasks.map((item) => {
     const bgColor: string = item.status ? "bg-emerald-200" : "bg-stone-200";
     const imageChoice = item.status ? image.checkmark : image.notdone;
     return (
       <button
         className={`flex flex-row p-2 gap-3 items-center ${bgColor} rounded-lg break-all cursor-pointer hover:shadow-lg`}
         key={item.name.toLowerCase()}
-        onClick={(e) => taskHandler(e, item.id)}
+        onClick={(e) => onToggle(e, item.id)}
       >
         <img src={imageChoice} alt="" className="rounded-md" />
         <span className="">{item.name}</span>
