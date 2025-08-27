98388a7e2fa788ea8cc2a4f670226ca3ec3ee084
kolvra <sabrisirajkholbiabby@gmail.com>
2025-08-27 15:34:49 +0700

    item status logic

--- src/Components/Task.tsx
+++ src/Components/Task.tsx
@@ -1,33 +1,42 @@
+import { useState, type MouseEvent } from "react";
+
 function Task() {
-  const taskItems = [
+  const [taskItems, setTaskItems] = useState([
     {
       name: "abby",
       status: false,
+      id: 1,
     },
     {
-      name: "nadia",
+      name: "Nadia",
       status: true,
+      id: 2,
     },
-  ];
+  ]);
 
   const image = {
     checkmark: "../public/images/checkmark.svg",
     notdone: "../public/images/notdone.svg",
   };
 
-  function taskHandler (){
-    
+  function taskHandler(event: MouseEvent<HTMLButtonElement>, id: number) {
+    console.log(event.currentTarget, id);
+    setTaskItems((prev) =>
+      prev.map((item) => {
+        if (item.id === id) return { ...item, status: !item.status };
+        return item;
+      })
+    );
   }
 
   const itemList = taskItems.map((item) => {
-    console.log(`${item.status} ${item.name}`);
     const bgColor: string = item.status ? "bg-emerald-200" : "bg-stone-200";
     const imageChoice = item.status ? image.checkmark : image.notdone;
     return (
       <button
         className={`flex flex-row p-2 gap-3 items-center ${bgColor} rounded-lg break-all cursor-pointer hover:shadow-lg`}
         key={item.name.toLowerCase()}
-        onClick={taskHandler}
+        onClick={(e) => taskHandler(e, item.id)}
       >
         <img src={imageChoice} alt="" className="rounded-md" />
         <span className="">{item.name}</span>
