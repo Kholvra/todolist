5908511715771c134c0ec244c527beaced69cf95
kolvra <sabrisirajkholbiabby@gmail.com>
2025-08-27 11:03:44 +0700

    init file

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Task from "./Components/Task";
+
+function App() {
+  return (
+    <>
+      <Task />
+      <h1 className="hover:scale-150">halo</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Task.tsx
@@ -0,0 +1,39 @@
+function Task() {
+  const taskItems = [
+    {
+      name: "abby",
+      status: false,
+    },
+    {
+      name: "nadia",
+      status: true,
+    },
+  ];
+
+  const image = {
+    checkmark: "../public/images/checkmark.svg",
+    notdone: "../public/images/notdone.svg",
+  };
+
+  function taskHandler (){
+  }
+
+  const itemList = taskItems.map((item) => {
+    console.log(`${item.status} ${item.name}`);
+    const bgColor: string = item.status ? "bg-emerald-200" : "bg-stone-200";
+    const imageChoice = item.status ? image.checkmark : image.notdone;
+    return (
+      <button
+        className={`flex flex-row p-2 gap-3 items-center ${bgColor} rounded-lg break-all cursor-pointer hover:shadow-lg`}
+        key={item.name.toLowerCase()}
+        onClick={taskHandler}
+      >
+        <img src={imageChoice} alt="" className="rounded-md" />
+        <span className="">{item.name}</span>
+      </button>
+    );
+  });
+  return <>{itemList}</>;
+}
+
+export default Task;
